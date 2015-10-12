4d961e7f3b3e0ef5df348f50ef0e3e54d1b93983
liyatang <liyatang@xiaonongnv.com>
2015-10-12 13:09:44 +0800

    增加两个组件 grid  pagination

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import  Grid from './grid.component.js';
+import  Grid from './lib/grid.component.js';
 
 
 //let onClick = (() => console.log(arguments));
--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,21 @@
 var path = require('path');
 var webpack = require('webpack');
+//var CommonsChunkPlugin = require('webpack/lib/optimize/CommonsChunkPlugin');
 
 module.exports = {
     devtool: 'cheap-module-eval-source-map',
-    entry: [
-        'webpack-hot-middleware/client',
-        './static/index'
-    ],
+    entry: {
+        bundle: './index'
+    },
     output: {
-        path: path.join(__dirname, 'dist'),
-        filename: 'bundle.js',
-        publicPath: '/static/'
+        path: path.join(__dirname, ''),
+        filename: '[name].js',
+        publicPath: '/'
     },
     plugins: [
         new webpack.HotModuleReplacementPlugin(),
         new webpack.NoErrorsPlugin()
+        //new CommonsChunkPlugin('common.js')
     ],
     module: {
         loaders: [{
