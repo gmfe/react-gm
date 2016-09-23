d5f14b2ac3ddc537f011c59b23277cb642597659
zhangsiqi <276987359@qq.com>
2016-09-23 15:43:40 +0800

    c

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,5 +2,9 @@ module.exports = {
     "plugins": [
         "gm"
     ],
-    "extends": ["plugin:gm/recommended"]
+    "extends": ["plugin:gm/recommended"],
+    "rules": {
+        "react/no-string-refs": 0,
+        "react/no-find-dom-node": 0
+    }
 };
