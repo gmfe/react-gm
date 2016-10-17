29d0234e6653005a8036924d5c79f2eb42648a24
liyatang <liyatang@xiaonongnv.com>
2016-10-17 16:04:35 +0800

    c

--- dist/react-gm.js
+++ dist/react-gm.js
@@ -5557,6 +5557,10 @@ return /******/ (function(modules) { // webpackBootstrap
 
 	'use strict';
 
+	Object.defineProperty(exports, "__esModule", {
+	    value: true
+	});
+
 	var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
 
 	var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
@@ -5698,13 +5702,17 @@ return /******/ (function(modules) { // webpackBootstrap
 	    onChange: noop
 	};
 
-	module.exports = Switch;
+	exports.default = Switch;
 
 /***/ },
 /* 44 */
 /***/ function(module, exports, __webpack_require__) {
 
 	'use strict';
+
+	Object.defineProperty(exports, "__esModule", {
+	    value: true
+	});
 
 	var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
 
@@ -5867,7 +5875,7 @@ return /******/ (function(modules) { // webpackBootstrap
 	    onChange: noop
 	};
 
-	module.exports = Switcher;
+	exports.default = Switcher;
 
 /***/ },
 /* 45 */
