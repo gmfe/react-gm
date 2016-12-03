b28d6d41d1cb3ea618b8cc5c5abe057a95cdb8ff
liyatang <liyatang@xiaonongnv.com>
2016-12-03 11:48:11 +0800

    gh-pages

--- deploy/gh-pages.sh
+++ deploy/gh-pages.sh
@@ -5,8 +5,8 @@ git checkout -b gh-pages;
 NODE_ENV=production webpack --config webpack.config.dev.js -p;
 rm -rf .gitignore;
 rm -rf .idea;
-rm -rf .happypack
+rm -rf .happypack;
 git add --all;
-git commit -m 'gh-page';
-git push origin HEAD:gh-pages -f;
+git commit -m 'gh-pages';
+git push origin gh-pages:gh-pages -f;
 git checkout master;
