12dea2591da07873bd45e460e76f44806e141d67
zhangsiqi <276987359@qq.com>
2016-12-26 16:58:39 +0800

    doc generate

--- deploy/gh-pages.sh
+++ deploy/gh-pages.sh
@@ -1,12 +1,20 @@
 #! /bin/sh
 
-git branch -D gh-pages;
-git checkout -b gh-pages;
+#git branch -D gh-pages;
+#git checkout -b gh-pages;
+#NODE_ENV=production webpack --config webpack.config.dev.js -p;
+#rm -rf .gitignore;
+#rm -rf .idea;
+#rm -rf .happypack;
+#git add --all;
+#git commit -m 'gh-pages';
+#git push origin gh-pages:gh-pages -f;
+#git checkout master;
+
+rm -rf doc;
+mkdir doc;
 NODE_ENV=production webpack --config webpack.config.dev.js -p;
-rm -rf .gitignore;
-rm -rf .idea;
-rm -rf .happypack;
-git add --all;
-git commit -m 'gh-pages';
-git push origin gh-pages:gh-pages -f;
-git checkout master;
+cp index.html doc;
+cp -r build doc;
+git commit -am 'doc generate';
+git push;
