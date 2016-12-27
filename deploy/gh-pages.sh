c0d64743d0197318a1d0f6438a7d486032c1a128
zhangsiqi <276987359@qq.com>
2016-12-27 11:17:31 +0800

    docs generate

--- deploy/gh-pages.sh
+++ deploy/gh-pages.sh
@@ -5,6 +5,6 @@ mkdir docs;
 NODE_ENV=production webpack --config webpack.config.dev.js -p;
 cp index.html docs;
 cp -r build docs;
-git commit --all;
+git add --all;
 git commit -m 'docs generate';
 git push;
