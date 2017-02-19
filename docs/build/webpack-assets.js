b0f8f4e7eb20c651da3ecd7a25c625c05dbd66ab
liyatang <liyatang@xiaonongnv.com>
2017-02-19 14:00:35 +0800

    docs generate

--- docs/build/webpack-assets.js
+++ docs/build/webpack-assets.js
@@ -1,1 +1,1 @@
-window.WEBPACK_ASSETS = {"index":{"js":"/react-gm/build/index.6fb688a0d1c25a0b3086.bundle.js"}}
+window.WEBPACK_ASSETS = {"index":{"js":"/react-gm/build/index.93a43edc6fee005f1b89.bundle.js"}}
