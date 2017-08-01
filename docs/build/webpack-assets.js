aa303d5e19fadf247c36d619f16188a2d13e00c0
maltose <Page.qy@test.test>
2017-08-01 14:48:32 +0800

    docs generate

--- docs/build/webpack-assets.js
+++ docs/build/webpack-assets.js
@@ -1,1 +1,1 @@
-window.WEBPACK_ASSETS = {"index":{"js":"/react-gm/build/index.839415dca687df4028bf.bundle.js"}}
+window.WEBPACK_ASSETS = {"index":{"js":"/react-gm/build/index.b20cbca1b8a4d64a34f5.bundle.js"}}
