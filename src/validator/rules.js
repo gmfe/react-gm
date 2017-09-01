c7aced9e98c13fe7874fbdab20e1b5837dccec2a
maltose <liyatang@xiaonongnv.com>
2017-09-01 14:24:05 +0800

    fix number_or_letter

--- src/validator/rules.js
+++ src/validator/rules.js
@@ -6,7 +6,7 @@ const pattern = {
     url: new RegExp('^(?!mailto:)(?:(?:http|https|ftp)://|//)(?:\\S+(?::\\S*)?@)?(?:(?:(?:[1-9]\\d?|1\\d\\d|2[01]\\d|22[0-3])(?:\\.(?:1?\\d{1,2}|2[0-4]\\d|25[0-5])){2}(?:\\.(?:[0-9]\\d?|1\\d\\d|2[0-4]\\d|25[0-4]))|(?:(?:[a-z\\u00a1-\\uffff0-9]+-?)*[a-z\\u00a1-\\uffff0-9]+)(?:\\.(?:[a-z\\u00a1-\\uffff0-9]+-?)*[a-z\\u00a1-\\uffff0-9]+)*(?:\\.(?:[a-z\\u00a1-\\uffff]{2,})))|localhost)(?::\\d{2,5})?(?:(/|\\?|#)[^\\s]*)?$', 'i'),
     number: /^[+-]?((\d+.\d+)|(\d+))$/,
     number_positive: /^((\d+.\d+)|(\d+))$/,
-    number_or_letter: /^[a-zA-Z0-9]$/
+    number_or_letter: /^[a-zA-Z0-9]+$/
 };
 
 Validator.register(TYPE.required, [{
