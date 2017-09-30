5480b1515670bea5325d0bf744a0f036280b7ca2
Chaoming <1173306360@qq.com>
2017-09-30 16:50:53 +0800

    1. 代码优化  2.更换demo素材图片  3.更新文档

--- src/component/image_preview/index.js
+++ src/component/image_preview/index.js
@@ -1,23 +1,21 @@
-import React from 'react';
-import PreviewModal from './preview_modal';
-import Modal from '../modal';
+import React from "react";
+import PreviewModal from "./preview_modal";
+import Modal from "../modal";
 
-const ImagePreview = (props) => {
-
-  Modal.render({
-    children: <PreviewModal {...props} onHide={Modal.hide} />,
-    clean: true,
-    show: true,
-    disableMaskClose: true,
-    style: {
-      width: 'auto',
-      background: 'black',
-      margin: '50px',
-      height: 'calc(100% - 100px)'
-    }
-  });
-
+const ImagePreview = props => {
+    Modal.render({
+        children: <PreviewModal {...props} onHide={Modal.hide} />,
+        clean: true,
+        show: true,
+        onHide: Modal.hide,
+        disableMaskClose: true,
+        style: {
+            width: "auto",
+            background: "black",
+            margin: "50px",
+            height: "calc(100% - 100px)"
+        }
+    });
 };
-
 
 export default ImagePreview;
