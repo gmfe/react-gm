ac6e0cfb451b458aa56e8ed6ea584f449bcacf0a
Chaoming <1173306360@qq.com>
2017-11-09 12:01:03 +0800

    改为更为合理的修改padding

--- src/component/modal/index.js
+++ src/component/modal/index.js
@@ -133,7 +133,7 @@ class Modal extends React.Component {
                                 {title}
                             </div>
                         ) : null}
-                        <div className={noContentPadding ? "gm-padding-0" : "gm-modal-content"}>
+                        <div className={classNames("gm-modal-content", {"gm-modal-content-padding-0": noContentPadding})}>
                             {children}
                         </div>
                     </div>
