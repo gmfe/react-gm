4d59dbcf1251752a9abfd85e5171c19a411c4c58
maltose <liyatang@xiaonongnv.com>
2017-11-13 18:54:15 +0800

    废弃 publish.sh topContent 增加 fixedTop 特定样式添加  !important

--- deploy/publish.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/sh
-
-npm version patch;
-git commit -am "version patch";
-git push origin master:master;
-npm publish --registry='https://registry.npmjs.org';
-cnpm sync react-gm;
-npm version;
