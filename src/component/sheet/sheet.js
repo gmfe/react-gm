83eee347151344b1368b24d67694b6585b7f033c
liyatang <liyatang@qq.com>
2018-06-25 16:56:46 +0800

    fix issue

--- src/component/sheet/sheet.js
+++ src/component/sheet/sheet.js
@@ -138,7 +138,6 @@ class Sheet extends React.Component {
 
             if (expandedRowRender && value.__gm_expanded) {
                 trs.push(<tr className="gm-sheet-expanded-tr" key={index + '_expanded'}>
-                    <td/>
                     <td className="gm-sheet-expanded-td" colSpan="99">
                         {expandedRowRender(index)}
                     </td>
