e5c351f18ba3b84f6de76e3a15c5dcecbb02358a
chenzesam <chenzesam@gmail.com>
2018-07-12 14:21:03 +0800

    @hotfix sheet 组件(#244)
    @desc 1. sheet 的 children 有可能是 false 等特殊值(和 Form 组件类似), 将其转化为数组的方式使用 React.Children.toArray 替换, 可以将特殊值替换掉;
    @desc 2. SheetColumn 目前可以接受两个参数 value(对应 field 的数据) 和 index(在数据中的索引), 但是在业务代码中大量出现 list[index] 的方式去获取该条数据下的其他 field(在数据中的索引), 故引入第三个参数 record(该条数行数据)

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,9 @@
-const autoprefixer = require('autoprefixer');
-const precss = require('precss');
+const autoprefixer = require('autoprefixer')
+const precss = require('precss')
 
 module.exports = {
-    plugins: [
-        autoprefixer({browsers: ['iOS >= 8', 'Android >= 4.1']}),
-        precss
-    ]
-};
+  plugins: [
+    autoprefixer({browsers: ['iOS >= 8', 'Android >= 4.1']}),
+    precss
+  ]
+}
