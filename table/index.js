ad28c1775daa3b6c2e8350de785445119fea85bf
liyatang <liyatang@qq.com>
2018-07-26 19:01:27 +0800

    table fix

--- table/index.js
+++ table/index.js
@@ -1,11 +1,19 @@
+import React from 'react'
 import Table from './table'
 import SelectTable from './select_table'
 import ExpandTable from './expand_table'
 import 'react-table/react-table.css'
 import './style.less'
 
+const TableUtil = {
+  OperationHeader: <div className='text-center'><i className='xfont xfont-fun' style={{color: 'rgb(19, 193, 159)'}}/>
+  </div>,
+  OperationCell: (props) => <div {...props} className={'text-center ' + props.className}/>
+}
+
 export {
   Table,
   ExpandTable,
-  SelectTable
+  SelectTable,
+  TableUtil
 }
