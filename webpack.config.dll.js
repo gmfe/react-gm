d5163dbc977ed5907929227838b0ec4250007d89
liyatang <liyatang@qq.com>
2018-07-28 22:49:04 +0800

    加快

--- webpack.config.dll.js
+++ webpack.config.dll.js
@@ -4,11 +4,13 @@ const webpackConfigDll = require('gm-webpack/webpack.config.dll.js')
 const config = webpackConfigDll({
   publicPath: '/react-gm/build/',
   dll: [
-    'react', 'react-dom',
+    'react', 'react-dom', 'prop-types',
     'classnames',
     'react-router', 'react-router-dom',
     'history', 'query-string',
-    'lodash', 'moment'
+    'lodash', 'moment',
+    'gm-util',
+    'react-table'
   ]
 })
 
