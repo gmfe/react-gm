0c4cfcfaac8836ca62b39d67d769c8a2467f240f
hanzhibang <354172676@qq.com>
2019-01-10 14:52:58 +0800

    Affix用sticky实现

--- demo/doc.nav.config.js
+++ demo/doc.nav.config.js
@@ -48,7 +48,8 @@ const data = [{
     { name: 'Popover 浮层', path: '/doc/Popover' },
     { name: 'Modal 模态框', path: '/doc/Modal' },
     { name: 'Drawer 抽屉', path: '/doc/Drawer' },
-    { name: 'ImagePreview 图片预览', path: '/doc/ImagePreview' }
+    { name: 'ImagePreview 图片预览', path: '/doc/ImagePreview' },
+    { name: 'Affix 粘性布局', path: '/doc/Affix' }
   ]
 }, {
   name: 'Form',
--- src/component/affix/index.js
+++ src/component/affix/index.js
@@ -1,76 +1,22 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import ReactDOM from 'react-dom'
-import _ from 'lodash'
 
 class Affix extends React.Component {
   constructor (props) {
     super(props)
 
-    this.state = {
-      boxStyle: {},
-      style: {}
-    }
-
-    this.throttleHandleScroll = _.throttle(this.handleScroll, 500)
-  }
-
-  componentDidMount () {
-    window.addEventListener('scroll', this.throttleHandleScroll)
-    this.dom = ReactDOM.findDOMNode(this)
-  }
-
-  componentWillUnmount () {
-    window.removeEventListener('scroll', this.throttleHandleScroll)
-  }
-
-  handleScroll = () => {
-    const rect = this.dom.getBoundingClientRect()
-    const { top, bottom, offset } = this.props
-
-    if (top && rect.top - offset <= 0) {
-      this.setState({
-        boxStyle: {
-          width: rect.width,
-          height: rect.height
-        },
-        style: {
-          position: 'fixed',
-          top: offset + 'px',
-          left: rect.left,
-          width: rect.width
-        }
-      })
-    } else if (bottom && rect.bottom + offset >= (window.innerHeight || window.document.documentElement.clientHeight)) {
-      this.setState({
-        boxStyle: {
-          width: rect.width,
-          height: rect.height
-        },
-        style: {
-          position: 'fixed',
-          bottom: offset + 'px',
-          left: rect.left,
-          width: rect.width
-        }
-      })
-    } else {
-      this.setState({
-        boxStyle: {},
-        style: {}
-      })
+    this.style = {
+        position: 'sticky'
     }
   }
 
   render () {
-    const { children } = this.props
-    const { style, boxStyle } = this.state
-
+    const { children, offset, top, bottom } = this.props
+    top ? this.style.top = `${offset}px` : null
+    bottom ? this.style.bottom = `${offset}px` : null
     return (
-      <div style={boxStyle}>
-        <div style={style}>
+      <div style={this.style}>
           {children}
-        </div>
       </div>
     )
   }
