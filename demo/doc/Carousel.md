fb5bbe6264dd71add7b83ca5a9268fe0f9dbb671
tubulang <1053736438standard@gmail.com>
2019-05-13 15:18:12 +0800

    modify carousel

--- demo/doc/Carousel.md
+++ demo/doc/Carousel.md
@@ -1,22 +1,10 @@
 ---
 imports:
-    import {Carousel} from '../../src/index';
+    import {Carousel, Flex} from '../../src/index';
 ---
 ## Carousel
-::: demo 以下example依赖的数据
-```js
-const imgData=[
-    'https://img.guanmai.cn/product_pic/da32d562de124462.png',
-    'https://img.guanmai.cn/product_pic/ec206dc3750fe865.jpeg',
-    'http://lorempixel.com/400/200',
-    '//img.guanmai.cn/station_pic/gm_banner.jpg',
-    '//img.guanmai.cn/station_pic/sha1_ef0537da1c01453dd680f7c558a12aa9d520354b.jpg',
-]
-const colorData = ['red','white','white','grey','pink']
-```
-:::
 
-Carousel 纯轮播(默认模式)
+Carousel 纯轮播(默认模式)(轮播区域自定义)
 
 ::: demo Carousel
 ```js
@@ -24,8 +12,18 @@ class CarouselExample extends React.Component {
     render() {
         return (
         <Carousel
-          imgData={imgData}
-        />
+          style={{width:'1000px',height:'200px'}}
+        >
+            <div style={{backgroundColor: 'red', height:'100%',width:'100%'}}>
+               <Flex justifyCenter>
+                 <img style={{height:'200px',width:'800px'}} src='https://img.guanmai.cn/product_pic/da32d562de124462.png'/>
+               </Flex>
+            </div>
+            <div style={{backgroundColor: 'green', height:'100%'}}>showCarousel1</div>
+            <div style={{backgroundColor: 'yellow', height:'100%'}}>showCarousel2</div>
+            <div style={{backgroundColor: 'blue', height:'100%'}}>showCarousel3</div>
+            <div style={{backgroundColor: 'orange', height:'100%'}}>showCarousel4</div>
+        </Carousel>
         )
     }
 }
@@ -36,80 +34,13 @@ class CarouselExample extends React.Component {
 ```
 :::
 
-Carousel 滑动模式
-::: demo Carousel
-```js
-class CarouselExample1 extends React.Component {
-    render() {
-        return (
-        <Carousel
-          imgData={imgData}
-          switchType='move'
-        />
-        )
-    }
-}
-```
-
-```jsx
-<CarouselExample1/>
-```
-:::
-
-Carousel 点击事件,背景颜色, 显示控制按钮
-::: demo Carousel
-```js
-class CarouselExample2 extends React.Component {
-    handleClick = (index) => {
-        alert(index)
-    }
-    render() {
-        return (
-        <Carousel
-          imgData={imgData}
-          colorData={colorData}
-          onSelect={this.handleClick} 
-          hasSideSwitch         
-        />
-        )
-    }
-}
-```
-
-```jsx
-<CarouselExample2/>
-```
-:::
-
 ### Props
-- `width（string）` 轮播区域宽度 
-- `height (string)` 轮播区域高度
-- `imgData` 轮播图数据 (array) 格式：`{'[\'\',\'\']'}`
-- `colorData` 对应的背景颜色数据 (array) 格式：`{'[\'\',\'\']'}`
-- `switchType（string）` 轮播类型 （'move','fade'）例：switchType='move'
 - `delay（number）` 轮播间隔时间
-- `speed（number）` 单个切换过程时间（ms） 
-- `onSelect （func）` 点击事件 
+- `transitionTime（number）` 单个切换过程时间（ms）  
 - `defaultIndex（number）` 设置初始索引
-- `isAutoPlay（bool）` 是否自动轮播 
-- `isStopCarousel（bool）` 鼠标悬空和点击取消轮播，鼠标离开则继续轮播（需开启轮播才有效）
-- `hasSideSwitch（bool）` 是否显示左右控制 
-- `hasFooterSwitch（bool）` 是否显示下标控制 
-- `className` 自定义样式 
 - `...rest` 
 
 ### Default Props
-- `width`: '500px', 轮播区域宽度
-- `height`: '300px', 轮播区域高度
-- `switchType`: 'fade', 轮播类型
-- `delay`: 7000, 轮播间隔时间
-- `speed`: 2000, 单个切换过程时间（ms）
-- `onSelect`: _.noop, 点击事件
-- `isAutoPlay`: true, 是否自动轮播
+- `delay`: 7000, 轮播间隔时间（ms）
+- `transitionTime`: 2000, 单个切换过程时间（ms）
 - `defaultIndex`: 0, 设置初始索引
-- `isStopCarousel`: true, 鼠标悬空和点击取消轮播，鼠标离开则开始轮播
-- `hasSideSwitch`: false, 是否显示左右控制
-- `hasFooterSwitch`: true 是否显示下标控制
-
-### Tips
-- 建议`delay`的值大于`speed`的值，不然`move`模式下会出现意想不到的轮播顺序
--- src/component/carousel/carousel.js
+++ src/component/carousel/carousel.js
@@ -1,266 +1,137 @@
 import React from 'react'
-import _ from 'lodash'
 import classNames from 'classnames'
-import PropTypes from 'prop-types'
 import Flex from '../flex'
+import PropTypes from 'prop-types'
 
 class Carousel extends React.Component {
   constructor (props) {
     super(props)
+
     this.state = {
-      currentIndex: props.defaultIndex,
-      transitionTime: props.speed,
-      size: props.imgData.length
+      currentIndex: props.defaultIndex
     }
 
     this.timer = null
   }
 
   componentDidMount () {
-    const { imgData, delay, isAutoPlay } = this.props
-
-    if (imgData.length > 1 && isAutoPlay) {
-      this.timer = setTimeout(this.handleSlider, delay)
+    if (React.Children.count(this.props.children) > 0) {
+      this.startCarousel()
     }
   }
 
   componentWillUnmount () {
-    if (this.timer) {
-      clearTimeout(this.timer)
-    }
+    clearInterval(this.timer)
   }
 
-  handleSlider = () => {
-    const { speed, switchType, delay } = this.props
-    const { size, currentIndex } = this.state
-    clearTimeout(this.timer)
+  setCurrentIndex = () => {
+    let index = this.state.currentIndex
 
-    if (currentIndex >= (size - 1) && switchType === 'fade') {
-      // 取消动画，趁别人不注意切换到第一个
-      this.setState({
-        currentIndex: 0,
-        transitionTime: speed
-      })
-    } else if (currentIndex >= size && switchType === 'move') { // 滑动操作
-      this.setState({
-        transitionTime: 0,
-        currentIndex: 0
-      }, () => {
-        setTimeout(() => {
-          this.setState({ transitionTime: speed, currentIndex: 1 })
-        }, 20)
-      })// 原理为当前是用第一张作为最后一张图时，通过直接切换到第一张，实现下次加一为0+1
+    if (index === React.Children.count(this.props.children) - 1) {
+      index = 0
     } else {
-      this.setState({
-        transitionTime: speed,
-        currentIndex: currentIndex + 1
-      })
+      index++
     }
-
-    this.timer = setTimeout(this.handleSlider, delay)
-  }
-
-  handleSelect (index) {
-    const { speed } = this.props
-    this.props.isAutoPlay && clearTimeout(this.timer)
 
     this.setState({
-      currentIndex: index,
-      transitionTime: speed
+      currentIndex: index
     })
+  }
 
-    if (this.props.isAutoPlay && !this.props.isStopCarousel) {
-      this.timer = setTimeout(this.handleSlider, this.props.delay)
-    }
+  startCarousel = () => {
+    const { delay } = this.props
+
+    this.timer = setInterval(this.setCurrentIndex, delay)
+  }
+
+  stopCarousel = () => {
+    clearInterval(this.timer)
+  }
+
+  handleSelect = index => {
+    this.stopCarousel()
+
+    this.setState({
+      currentIndex: index
+    })
   }
 
   handleCancelSelect = () => {
-    if (this.props.isAutoPlay) {
-      this.timer = setTimeout(this.handleSlider, this.props.delay)
-    }
+    this.startCarousel()
   }
 
-  handleControl (type) {
-    const { delay, speed } = this.props
-    const { size } = this.state
-    let changeIndex = size
-    let targetIndex = this.state.currentIndex
-
-    clearTimeout(this.timer)
-
-    if (this.state.currentIndex <= 0 || this.state.currentIndex >= size) {
-      type === 'left' ? changeIndex = size : changeIndex = 0
-      this.setState({
-        transitionTime: 0,
-        currentIndex: changeIndex
-      }, () => {
-        setTimeout(() => {
-          type === 'left' ? --changeIndex : ++changeIndex
-          this.setState({
-            transitionTime: speed,
-            currentIndex: changeIndex
-          })
-        }, 0)
-      })
-    } else {
-      type === 'left' ? --targetIndex : ++targetIndex
-      this.setState({
-        currentIndex: targetIndex,
-        transitionTime: speed
-      })
+  renderChildren = () => {
+    const { children, transitionTime } = this.props
+    const { currentIndex } = this.state
+    const fadeTransitionStyle = {
+      transition: `all ${transitionTime}ms ease-in-out`
     }
 
-    if (this.props.isAutoPlay) {
-      this.timer = setTimeout(this.handleSlider, delay)
-    }
+    return React.Children.map(children, (thisArg, index) => {
+      return React.cloneElement(thisArg, {
+        style: Object.assign({}, thisArg.props.style, fadeTransitionStyle),
+        className: classNames('gm-carousel-fade-item',
+          { 'gm-carousel-fade-item-active': currentIndex === index },
+          thisArg.props.className
+        ),
+        key: index
+      })
+    })
   }
 
-  handleClick = e => {
-    e.preventDefault()
-    this.props.onSelect(this.state.currentIndex >= this.state.size ? 0 : this.state.currentIndex)
+  renderFooterController = () => {
+    const { children } = this.props
+    const { currentIndex } = this.state
+
+    return <ul className={'gm-carousel-fade-footer'}>
+      {
+        React.Children.map(children, (value, index) => {
+          return <li
+            className={
+              classNames(
+                { 'gm-carousel-fade-footer-li-hover': currentIndex === index },
+                'gm-carousel-fade-footer-li'
+              )
+            }
+            key={index}
+            onMouseOver={this.handleSelect.bind(this, index)}
+            onMouseLeave={this.handleCancelSelect}
+          />
+        })
+      }
+    </ul>
   }
 
   render () {
-    const { currentIndex, transitionTime, size } = this.state
-    const {
-      width,
-      height,
-      imgData,
-      colorData,
-      hasSideSwitch,
-      hasFooterSwitch,
-      switchType,
-      className,
-      hasBGColor,
-      onSelect, isAutoPlay, defaultIndex, isStopCarousel, // eslint-disable-line
-      ...rest
-    } = this.props
-
-    const currentColorIndex = currentIndex < imgData.length ? currentIndex : 0
-
-    const fadeTransitionStyle = switchType === 'fade' ? {
-      transition: `all ${transitionTime}ms ease-in-out`,
-      height: '100%'
-    } : {}
-    const bgColorStyle = (colorData && colorData.length > 0) ? {
-      backgroundColor: `${colorData[ currentColorIndex ]}`,
-      transition: `all ${transitionTime}ms ease-in-out`
-    } : {
-      transition: `all ${transitionTime}ms ease-in-out`
-    }
-    const allItemsStyle = {
-      transition: `transform ${transitionTime}ms ease-in-out`,
-      height: '100%',
-      whiteSpace: 'nowrap',
-      transform: (switchType === 'move')
-        ? (`translateX(${-parseInt(width) * currentIndex}px)`)
-        : 'opx'
-    }
-    const contentStyle = {
-      position: 'relative',
-      width: width,
-      minWidth: width,
-      height: height,
-      overflow: 'hidden',
-      maxHeight: '100%'
-    }
-
-    let carouselData = imgData.slice()
-    if (switchType === 'move') {
-      carouselData.push(imgData[0])
-    }
+    const { className, transitionTime, defaultIndex, ...rest } = this.props
 
     return (
       <Flex
         justifyCenter
-        style={bgColorStyle}
-        className={classNames(
-          { 'gm-carousel-move': switchType === 'move' },
-          { 'gm-carousel-fade': switchType === 'fade' },
-          className
-        )}
+        className={classNames('gm-carousel-fade', className)}
+        onMouseOver={this.stopCarousel}
+        onMouseLeave={this.handleCancelSelect}
         {...rest}
       >
-        <div style={contentStyle}>
-          <div
-            style={allItemsStyle}
-            onClick={this.handleClick}
-            className={classNames({ 'gm-carousel-move-all': switchType === 'move' })}
-          >
-            {
-              _.map(carouselData, (v, index) => {
-                return (
-                  <div
-                    style={fadeTransitionStyle}
-                    key={index}
-                    className={classNames(
-                      { 'gm-carousel-active': (currentIndex >= size ? 0 : parseInt(currentIndex)) === index },
-                      'gm-carousel-item'
-                    )}
-                  >
-                    <a>
-                      <img className='gm-carousel-item-img' src={v}/>
-                    </a>
-                  </div>
-                )
-              })
-            }
-          </div>
-          {hasSideSwitch ? (
-            <div className='gm-carousel-control-slide'>
-              <i onClick={this.handleControl.bind(this, 'left')} className='xfont xfont-left-small gm-carousel-control-slide-left'/>
-              <i onClick={this.handleControl.bind(this, 'right')} className='xfont xfont-right-small gm-carousel-control-slide-right'/>
-            </div>
-          ) : ''}
-          {hasFooterSwitch && (
-            <ul className='gm-carousel-control-footer'>
-              {
-                _.map(_.range(size), v => {
-                  return <li
-                    className={classNames(
-                      { 'gm-carousel-control-footer-hover': (currentIndex >= size ? 0 : parseInt(currentIndex)) === v },
-                      'gm-carousel-control-footer-li'
-                    )}
-                    key={v}
-                    onMouseOver={this.handleSelect.bind(this, v)}
-                    onMouseLeave={this.handleCancelSelect}
-                  />
-                })
-              }
-            </ul>
-          )}
-        </div>
+        { this.renderChildren() }
+        { this.renderFooterController() }
       </Flex>
     )
   }
 }
 
 Carousel.propTypes = {
-  imgData: PropTypes.array.isRequired,
-  colorData: PropTypes.array,
-  width: PropTypes.string,
-  height: PropTypes.string,
-  switchType: PropTypes.oneOf(['move', 'fade']),
-  delay: PropTypes.number,
-  speed: PropTypes.number,
-  onSelect: PropTypes.func,
-  isAutoPlay: PropTypes.bool,
   defaultIndex: PropTypes.number,
-  isStopCarousel: PropTypes.bool,
-  hasSideSwitch: PropTypes.bool,
-  hasFooterSwitch: PropTypes.bool
+  delay: PropTypes.number,
+  transitionTime: PropTypes.number,
+  children: PropTypes.array,
+  className: PropTypes.string
 }
+
 Carousel.defaultProps = {
-  width: '500px', // 轮播区域宽度
-  height: '300px', // 轮播区域高度
-  switchType: 'fade', // 轮播类型
-  delay: 7000, // 轮播时延
-  speed: 2000, // 切换时间（ms）
-  onSelect: _.noop, // 点击事件
-  isAutoPlay: true, // 是否自动轮播
   defaultIndex: 0, // 设置初始索引
-  isStopCarousel: true, // 鼠标悬空和点击取消轮播，鼠标离开则开始轮播
-  hasSideSwitch: false, // 是否显示左右控制
-  hasFooterSwitch: true // 是否显示下标控制
+  delay: 3000, // 轮播时延
+  transitionTime: 1000 // 切换时间（ms）
 }
+
 export default Carousel
