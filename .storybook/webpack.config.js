d2c2653c65027b54b97aca2e1db8a5fdc7474037
liyatang <liyatang@qq.com>
2019-06-20 17:58:36 +0800

    storybook + addon-storysource

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -26,5 +26,11 @@ module.exports = ({ config }) => {
     ]
   })
 
+  config.module.rules.push({
+    test: /\.stories\.jsx?$/,
+    loaders: [require.resolve('@storybook/addon-storysource/loader')],
+    enforce: 'pre'
+  })
+
   return config
 }
