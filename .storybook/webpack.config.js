deff1fd82afbcdd6b3bccc94b2821a3fdf6a5189
HecateDK <minyillee@gmail.com>
2019-06-28 15:29:00 +0800

    edit table

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,7 +1,7 @@
 module.exports = ({ config }) => {
   // 不知道正则怎么写，用 function 代替
   config.module.rules[0].exclude = function(filepath) {
-    if (filepath.includes('/node_modules/gm-util/')) {
+    if (filepath.includes('/node_modules/gm-util/') || filepath.includes('/node_modules/gm-svg/')) {
       return false
     }
 
--- table/index.js
+++ table/index.js
@@ -5,12 +5,18 @@ import diyTableHOC from './diy_table'
 import ExpandSelectTable from './expand_select_table'
 import 'react-table/react-table.css'
 import './style.less'
-import { OperationHeader, OperationCell, SortHeader } from './util'
+import {
+  OperationHeader,
+  OperationCell,
+  SortHeader,
+  EditTableOperation
+} from './util'
 
 const TableUtil = {
   OperationHeader,
   OperationCell,
-  SortHeader
+  SortHeader,
+  EditTableOperation
 }
 
 export {
