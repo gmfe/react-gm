42d8279b5fff265b6f20d3d8e4d6f438ce421c5a
xumeimei <xumeimei@guanmai.cn>
2019-07-02 14:21:35 +0800

    timespan

---
+++ src/component/time_span/time_span.stories.js
@@ -0,0 +1,53 @@
+import React from 'react'
+import { storiesOf } from '@storybook/react'
+import TimeSpan from './time_span'
+import moment from 'moment'
+import { observable } from 'mobx'
+
+const timeSpanStatus = {
+  date: moment()
+    .startOf('day')
+    .toDate(),
+  setDate(date) {
+    this.date = date
+  }
+}
+
+const store1 = observable(timeSpanStatus)
+const store2 = observable(timeSpanStatus)
+const store3 = observable(timeSpanStatus)
+
+storiesOf('TimeSpan', module)
+  .add('不设置时间最大值 && 禁用某个时间段', () => (
+    <TimeSpan
+      max={null}
+      disabledSpan={spanMoment =>
+        spanMoment.isSameOrAfter(moment('11:00', 'HH:mm')) &&
+        spanMoment.isSameOrBefore(moment('18:30', 'HH:mm'))
+      }
+      selected={store1.date}
+      onSelect={date => store1.setDate(date)}
+    />
+  ))
+
+  .add('设置最大时间值为 20:00', () => (
+    <TimeSpan
+      max={moment()
+        .hour(20)
+        .minute(0)}
+      selected={store2.date}
+      onSelect={date => store2.setDate(date)}
+    />
+  ))
+
+  .add('自定义时间跨度为 1 小时（默认 30 分钟），展示格式为 HH', () => (
+    <TimeSpan
+      max={moment()
+        .hour(23)
+        .minute(0)}
+      span={60 * 60 * 1000}
+      render={value => moment(value).format('HH')}
+      selected={store3.date}
+      onSelect={date => store3.setDate(date)}
+    />
+  ))
