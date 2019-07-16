54bb310d0692a8b230334ca988948a4917a9ce5a
liyatang <liyatang@qq.com>
2019-07-16 11:50:28 +0800

    fix diy

--- table/util.js
+++ table/util.js
@@ -101,6 +101,10 @@ function getColumnKey(column) {
   else if (_.isFunction(column.accessor) && column.id) {
     return column.id
   }
+  // 额外的情况，有些时候只有id，比如 diy 存储就只存了 id，因为 函数没法存储
+  else if (column.id) {
+    return column.id
+  }
 
   // 其他情况没法获得 key
   return null
