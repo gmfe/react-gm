d9aaf449f597abc54f46e1855a94d7adc91a7c3c
liyatang <liyatang@qq.com>
2019-07-19 14:22:22 +0800

    babel

--- webpack.config.dll.js
+++ webpack.config.dll.js
@@ -3,12 +3,17 @@ const webpackConfigDll = require('gm-webpack/webpack.config.dll.js')
 const config = webpackConfigDll({
   publicPath: '/react-gm/build/',
   dll: [
-    'react', 'react-dom', 'prop-types',
+    'react',
+    'react-dom',
+    'prop-types',
     'classnames',
-    'react-router', 'react-router-dom',
-    'history', 'query-string',
-    'lodash', 'moment',
-    'gm-util',
+    'react-router',
+    'react-router-dom',
+    'history',
+    'query-string',
+    'lodash',
+    'moment',
+    'big.js',
     'react-table'
   ]
 })
