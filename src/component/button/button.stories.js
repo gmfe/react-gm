d10cab7f5dcd2969dc969b475d8e22098fc1a867
liyatang <liyatang@qq.com>
2019-07-24 16:51:01 +0800

    9.x

--- src/component/button/button.stories.js
+++ src/component/button/button.stories.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { storiesOf } from '@storybook/react'
 import Button from './index'
 import { observable } from 'mobx'
+import SVGMore from '../../../svg/more.svg'
 
 const store = observable({
   count: 0,
@@ -12,12 +13,18 @@ const store = observable({
 
 storiesOf('Button', module)
   .add('default', () => (
-    <Button
-      className='btn btn-default'
-      onClick={() => new Promise(resolve => setTimeout(() => resolve(), 2000))}
-    >
-      点击显示 loading
-    </Button>
+    <div>
+      <SVGMore className='gm-padding-lr-15' />
+
+      <Button
+        className='btn btn-default'
+        onClick={() =>
+          new Promise(resolve => setTimeout(() => resolve(), 2000))
+        }
+      >
+        点击显示 loading
+      </Button>
+    </div>
   ))
   .add('with mobx', () => (
     <Button onClick={() => store.addCount()}>aaa{store.count}</Button>
