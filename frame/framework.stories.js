ad1c363c1a29832d66583d79ed931939cab01c9c
liyatang <liyatang@qq.com>
2019-07-26 16:23:23 +0800

    full tab

--- frame/framework.stories.js
+++ frame/framework.stories.js
@@ -1,18 +1,39 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
-import Info from './info'
+import { Framework, RightTop, Info, FullTab } from './index'
 
 storiesOf('框架|FrameWork', module).add('default', () => (
   <div>
-    <Info
-      more={[
-        {
-          text: 'adfasf',
-          onClick: () => {
-            console.log('123')
+    <Framework
+      leftWidth='100px'
+      menu={
+        <div
+          style={{ width: '100px', height: '100vh', backgroundColor: 'red' }}
+        >
+          adfafa
+        </div>
+      }
+      rightTop={
+        <RightTop
+          info={
+            <Info
+              more={[
+                {
+                  text: 'adfasf',
+                  onClick: () => {
+                    console.log('123')
+                  }
+                }
+              ]}
+            />
           }
-        }
-      ]}
-    />
+        />
+      }
+    >
+      <FullTab tabs={['按订单查看', '按商品查看']}>
+        <div>按订单查看</div>
+        <div>按商品查看</div>
+      </FullTab>
+    </Framework>
   </div>
 ))
