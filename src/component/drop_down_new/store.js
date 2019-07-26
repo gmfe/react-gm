47e1e71a011de8e256a6707b2a184b9fb159fa05
LaamGinghong <351390560@qq.com>
2019-07-26 18:15:56 +0800

    add onClick function to DropDownNewItem

--- src/component/drop_down_new/store.js
+++ src/component/drop_down_new/store.js
@@ -1,11 +1,20 @@
 import { observable, action } from 'mobx'
 
 class Store {
-  @observable width = 0
+  /**
+   * DropdownNew组件包裹的内容的实际宽度，当该宽度长于下拉Menu的实际宽度时，用此宽度替代Menu的宽度
+   * @type {number}
+   * @private
+   */
+  @observable _width = 0
 
   @action
   setWidth(e) {
-    this.width = e
+    this._width = e
+  }
+
+  get width() {
+    return this._width
   }
 }
 
