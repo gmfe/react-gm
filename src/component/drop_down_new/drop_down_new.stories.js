d0b0963bec68cd3d864364bc9edcf90a77d26f1c
LaamGinghong <351390560@qq.com>
2019-07-26 11:28:07 +0800

    完成DropdownNew包裹组件

--- src/component/drop_down_new/drop_down_new.stories.js
+++ src/component/drop_down_new/drop_down_new.stories.js
@@ -1,7 +1,22 @@
 import { storiesOf } from '@storybook/react'
 import React from 'react'
 import DropDownNew from './drop_down_new'
+import { ButtonGroup, Button } from '../button'
 
 storiesOf('DropDownNew', module).add('Common', () => (
-  <DropDownNew container='Hello world!' />
+  <ButtonGroup>
+    <Button>DropDown</Button>
+    <DropDownNew
+      overlay={
+        <ul>
+          <li>1</li>
+          <li>2</li>
+          <li>3</li>
+        </ul>
+      }
+      trigger='hover'
+    >
+      <Button>...</Button>
+    </DropDownNew>
+  </ButtonGroup>
 ))
