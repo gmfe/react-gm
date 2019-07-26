dbf9ec801ed555a7f91a2354aca946801748c958
LaamGinghong <351390560@qq.com>
2019-07-26 17:34:39 +0800

    finish drop_down_new_menu's style

--- src/component/drop_down_new/drop_down_new_menu.js
+++ src/component/drop_down_new/drop_down_new_menu.js
@@ -1,10 +1,42 @@
-import React, { Component } from 'react'
+import React, { Component, createRef } from 'react'
+import { store } from './store'
 
 class DropDownNewMenu extends Component {
+  constructor(props) {
+    super(props)
+    this.currentRef = createRef()
+    this.widthFlag = store.width
+    this.state = {
+      style: {
+        width: 0
+      },
+      flag: false
+    }
+  }
+
+  componentDidMount() {
+    if (this.widthFlag > this.currentRef.current.offsetWidth) {
+      this.setState({
+        style: {
+          width: `${this.widthFlag}px`
+        },
+        flag: true
+      })
+    }
+  }
+
   render() {
     const { children } = this.props
-
-    return <ul className='dropdown-new-menu'>{children}</ul>
+    const { style, flag } = this.state
+    return (
+      <ul
+        style={flag ? style : {}}
+        ref={this.currentRef}
+        className='dropdown-new-menu'
+      >
+        {children}
+      </ul>
+    )
   }
 }
 
