58494beef51f176e0e03a1a84bc383afdb3e0657
LaamGinghong <351390560@qq.com>
2019-07-29 17:16:43 +0800

    add auto style to DropDownNew

--- src/component/drop_down_new/drop_down_new.stories.js
+++ src/component/drop_down_new/drop_down_new.stories.js
@@ -29,23 +29,35 @@ storiesOf('DropDownNew', module)
       </DropDownNew>
     )
   })
-  .add('Button Group', () => (
-    <ButtonGroup>
-      <Button>DropDown</Button>
-      <DropDownNew
-        overlay={
-          <ul>
-            <li>1</li>
-            <li>2</li>
-            <li>3</li>
-          </ul>
-        }
-        trigger='hover'
-      >
-        <Button>...</Button>
-      </DropDownNew>
-    </ButtonGroup>
-  ))
+  .add('Button Group', () => {
+    const print = () => console.log(123)
+    return (
+      <ButtonGroup>
+        <Button>DropDown</Button>
+        <DropDownNew
+          trigger='click'
+          placement='bottomRight'
+          overlay={
+            <DropDownNewMenu>
+              <DropDownNewItem onClick={print}>
+                123123123123123123123123123123123
+              </DropDownNewItem>
+              <DropDownNewSubMenu title='234' disabled>
+                <DropDownNewItem onClick={print}>123123</DropDownNewItem>
+              </DropDownNewSubMenu>
+              <DropDownNewSubMenu title='111'>
+                <DropDownNewItem>234234</DropDownNewItem>
+                <DropDownNewItem disabled>111222</DropDownNewItem>
+                <DropDownNewItem>908012</DropDownNewItem>
+              </DropDownNewSubMenu>
+            </DropDownNewMenu>
+          }
+        >
+          <Button>...</Button>
+        </DropDownNew>
+      </ButtonGroup>
+    )
+  })
   .add('Sub menu', () => {
     const print = () => console.log(123)
     return (
