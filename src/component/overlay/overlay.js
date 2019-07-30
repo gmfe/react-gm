ea081e1dc48d31f716af1c370300003af7a7af77
LaamGinghong <351390560@qq.com>
2019-07-30 17:18:56 +0800

    add update in overlay

--- src/component/overlay/overlay.js
+++ src/component/overlay/overlay.js
@@ -1,23 +1,35 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+let overlayRef
+
 export default {
   dom: undefined,
-  create({ content, style }) {
+  create({ content, showMask }) {
     this.close()
     this.dom = document.createElement('div')
-    this.dom.setAttribute('id', 'overlay-container')
-    const JSXDom = (
-      <div>
-        <div className='overlay' style={style}>
-          {content}
-        </div>
-      </div>
-    )
+    showMask && this.dom.setAttribute('id', 'overlay-container')
+    const JSXDom = render(content)
     ReactDOM.render(JSXDom, this.dom)
     document.body.appendChild(this.dom)
+    return new Promise(resolve => resolve(overlayRef))
   },
   close() {
     this.dom && this.dom.remove()
+  },
+  update(content, style) {
+    const JSXDom = render(content, style)
+    ReactDOM.render(JSXDom, this.dom)
+    document.body.appendChild(this.dom)
   }
+}
+
+function render(content, style) {
+  return (
+    <div>
+      <div className='overlay' style={style} ref={ref => (overlayRef = ref)}>
+        {content}
+      </div>
+    </div>
+  )
 }
