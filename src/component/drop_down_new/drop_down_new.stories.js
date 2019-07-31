4ffd348e2caeca6c164e369b44be2f7a476c5ff1
LaamGinghong <351390560@qq.com>
2019-07-31 14:21:35 +0800

    add DropDownNew stories

--- src/component/drop_down_new/drop_down_new.stories.js
+++ src/component/drop_down_new/drop_down_new.stories.js
@@ -1,88 +1,129 @@
 import { storiesOf } from '@storybook/react'
-import React from 'react'
+import React, { useState } from 'react'
 import {
   DropDownNew,
   DropDownNewMenu,
   DropDownNewItem,
   DropDownNewSubMenu
 } from './index'
-import { ButtonGroup, Button } from '../button'
-import Icon from '../../../svg/down.svg'
+import { Button } from '../button'
 
+import _ from 'lodash'
+
+const print = e => console.log(e)
+const items = _.map([0, 1, 2], (item, index) => {
+  return (
+    <DropDownNewItem key={index} onClick={() => print(item)}>
+      No.{item} Item
+    </DropDownNewItem>
+  )
+})
 storiesOf('DropDownNew', module)
   .add('Common', () => {
-    const print = () => console.log(123)
     return (
-      <DropDownNew
-        overlay={
-          <DropDownNewMenu>
-            <DropDownNewItem onClick={print}>123</DropDownNewItem>
-            <DropDownNewItem disabled>234</DropDownNewItem>
-            <DropDownNewItem>345</DropDownNewItem>
-          </DropDownNewMenu>
-        }
-      >
-        <Button>
-          Dropdown
-          <Icon />
-        </Button>
-      </DropDownNew>
+      <div style={{ height: '300px', textAlign: 'center', marginTop: '50px' }}>
+        <DropDownNew
+          placement='bottomRight'
+          overlay={<DropDownNewMenu>{items}</DropDownNewMenu>}
+        >
+          <Button>如果item比包裹层短，会自动补宽</Button>
+        </DropDownNew>
+      </div>
     )
   })
-  .add('Button Group', () => {
-    const print = () => console.log(123)
+  .add('Disabled', () => {
+    const disabledItems = _.map([0, 1, 2], (item, index) => {
+      return (
+        <DropDownNewItem
+          key={index}
+          onClick={() => print(item)}
+          disabled={index === 1}
+        >
+          No.{item} Item
+        </DropDownNewItem>
+      )
+    })
     return (
-      <ButtonGroup>
-        <Button>DropDown</Button>
+      <div style={{ height: '300px', textAlign: 'center', marginTop: '50px' }}>
+        <DropDownNew
+          overlay={<DropDownNewMenu>{items}</DropDownNewMenu>}
+          disabled
+        >
+          <Button>禁用</Button>
+        </DropDownNew>
+        <DropDownNew
+          overlay={<DropDownNewMenu>{disabledItems}</DropDownNewMenu>}
+        >
+          <Button>禁用Item</Button>
+        </DropDownNew>
+      </div>
+    )
+  })
+  .add('Placement', () => {
+    const buttons = [
+      'bottomLeft',
+      'bottomCenter',
+      'bottomRight',
+      'topLeft',
+      'topCenter',
+      'topRight'
+    ]
+    const [placement, changePlacement] = useState('bottomLeft')
+    return (
+      <div style={{ height: '300px', textAlign: 'center', marginTop: '50px' }}>
+        {_.map(buttons, (item, index) => {
+          return (
+            <Button key={index} onClick={() => changePlacement(item)}>
+              {item}
+            </Button>
+          )
+        })}
+        <br />
+        <DropDownNew
+          placement={placement}
+          overlay={<DropDownNewMenu>{items}</DropDownNewMenu>}
+        >
+          <Button>位置</Button>
+        </DropDownNew>
+      </div>
+    )
+  })
+  .add('trigger', () => {
+    return (
+      <div style={{ height: '300px', textAlign: 'center', marginTop: '50px' }}>
+        <DropDownNew overlay={<DropDownNewMenu>{items}</DropDownNewMenu>}>
+          <Button>Hover | Default</Button>
+        </DropDownNew>
         <DropDownNew
           trigger='click'
-          placement='bottomRight'
+          overlay={<DropDownNewMenu>{items}</DropDownNewMenu>}
+        >
+          <Button>Click</Button>
+        </DropDownNew>
+      </div>
+    )
+  })
+  .add('Cascading menu', () => {
+    return (
+      <div style={{ height: '300px', textAlign: 'center', marginTop: '50px' }}>
+        <DropDownNew
           overlay={
             <DropDownNewMenu>
-              <DropDownNewItem onClick={print}>
-                123123123123123123123123123123123
-              </DropDownNewItem>
-              <DropDownNewSubMenu title='234' disabled>
-                <DropDownNewItem onClick={print}>123123</DropDownNewItem>
+              <DropDownNewItem>No.1 Item</DropDownNewItem>
+              <DropDownNewSubMenu title='No.1 Submenu'>
+                <DropDownNewItem>No.2 Item</DropDownNewItem>
+                <DropDownNewItem disabled>No.3 Item</DropDownNewItem>
               </DropDownNewSubMenu>
-              <DropDownNewSubMenu title='111'>
-                <DropDownNewItem>234234</DropDownNewItem>
-                <DropDownNewItem disabled>111222</DropDownNewItem>
-                <DropDownNewItem>908012</DropDownNewItem>
+              <DropDownNewItem disabled>No.4 Item</DropDownNewItem>
+              <DropDownNewSubMenu title='No.2 Submenu' disabled>
+                <DropDownNewItem>No.5 Item</DropDownNewItem>
               </DropDownNewSubMenu>
             </DropDownNewMenu>
           }
         >
-          <Button>...</Button>
+          <Button>多级菜单</Button>
         </DropDownNew>
-      </ButtonGroup>
+      </div>
     )
   })
-  .add('Sub menu', () => {
-    const print = () => console.log(123)
-    return (
-      <DropDownNew
-        trigger='click'
-        overlay={
-          <DropDownNewMenu>
-            <DropDownNewItem onClick={print}>
-              123123123123123123123123123123123
-            </DropDownNewItem>
-            <DropDownNewSubMenu title='234' disabled>
-              <DropDownNewItem onClick={print}>123123</DropDownNewItem>
-            </DropDownNewSubMenu>
-            <DropDownNewSubMenu title='111'>
-              <DropDownNewItem>234234</DropDownNewItem>
-              <DropDownNewItem disabled>111222</DropDownNewItem>
-              <DropDownNewItem>908012</DropDownNewItem>
-            </DropDownNewSubMenu>
-          </DropDownNewMenu>
-        }
-      >
-        <Button>
-          Dropdown
-          <Icon />
-        </Button>
-      </DropDownNew>
-    )
-  })
+  .add('Button group', () => {})
