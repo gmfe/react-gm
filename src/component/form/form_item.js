e6ebc0e6a5e08c96ae90b4df755d6794939a4f79
liyatang <liyatang@qq.com>
2019-08-09 10:54:53 +0800

    form

--- src/component/form/form_item.js
+++ src/component/form/form_item.js
@@ -1,106 +1,106 @@
-import React, { useContext } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 import classNames from 'classnames'
 import Flex from '../flex'
 import _ from 'lodash'
 import Validator from '../../validator'
-import { WrapContext } from './util'
+import { withWrapContext } from './util'
 
 const FormControl = ({ children }) => {
   const { className } = children.props
 
-  const child = children
+  console.log(children)
 
   // 文件类型特殊
-  if (child.type === 'input' && child.props.type !== 'file') {
-    return React.cloneElement(child, {
-      className: classNames('form-control', className)
-    })
-  } else if (
-    child.type.displayName === 'textarea' ||
-    child.type.displayName === 'select' ||
-    child.type.displayName === 'InputNumber' ||
-    child.type.displayName === 'InputNumberV2'
+  if (
+    (children.type === 'input' && children.props.type !== 'file') ||
+    children.type === 'textarea' ||
+    children.type.displayName === 'select' ||
+    children.type.displayName === 'InputNumber' ||
+    children.type.displayName === 'InputNumberV2'
   ) {
-    return React.cloneElement(child, {
+    return React.cloneElement(children, {
       className: classNames('form-control', className)
     })
   }
 
-  return child
+  return children
 }
 
-const FormItemInner = ({
-  label,
-  labelWidth,
-  required,
-  canValidate,
-  validate,
-  error,
-  help,
-  unLabelTop,
-  className,
-  children,
-  ...rest
-}) => {
-  let hasLabelSwitchPaddingTop = false
-  if (canValidate && validate !== undefined) {
-    if (required) {
-      help = validate(function(value) {
-        return Validator.validate(Validator.TYPE.required, value)
-      })
-    } else {
-      help = validate()
+// 这种方式才会 storybook 才会显示 props 文档
+const FormItem = withWrapContext(
+  ({
+    label,
+    labelWidth,
+    required,
+    canValidate,
+    validate,
+    error,
+    help,
+    unLabelTop,
+    className,
+    children,
+    ...rest
+  }) => {
+    let hasLabelSwitchPaddingTop = false
+    if (canValidate && validate !== undefined) {
+      if (required) {
+        help = validate(function(value) {
+          return Validator.validate(Validator.TYPE.required, value)
+        })
+      } else {
+        help = validate()
+      }
+      error = !!help
     }
-    error = !!help
-  }
 
-  if (!_.isArray(children)) {
-    if (children.type.displayName === 'Switch') {
-      hasLabelSwitchPaddingTop = true
+    if (!_.isArray(children)) {
+      if (children.type.displayName === 'Switch') {
+        hasLabelSwitchPaddingTop = true
+      }
     }
-  }
 
-  return (
-    <Flex
-      {...rest}
-      className={classNames('gm-form-group', className, {
-        'has-error': error,
-        'gm-has-error': error
-      })}
-    >
-      {label && (
-        <Flex
-          justifyEnd
-          width={labelWidth}
-          className={classNames(
-            'gm-form-label control-label',
-            {
-              'gm-form-label-un-top': unLabelTop
-            },
-            {
-              'gm-form-label-switch-padding-top': hasLabelSwitchPaddingTop
-            }
-          )}
-        >
-          {required ? <span style={{ color: 'red' }}>*</span> : ''}
-          {label}
-          {label && '：'}
+    return (
+      <Flex
+        {...rest}
+        className={classNames('gm-form-group', className, {
+          'has-error': error,
+          'gm-has-error': error
+        })}
+      >
+        {label && (
+          <Flex
+            justifyEnd
+            width={labelWidth}
+            className={classNames(
+              'gm-form-label control-label',
+              {
+                'gm-form-label-un-top': unLabelTop
+              },
+              {
+                'gm-form-label-switch-padding-top': hasLabelSwitchPaddingTop
+              }
+            )}
+          >
+            {required ? <span style={{ color: 'red' }}>*</span> : ''}
+            {label}
+            {label && '：'}
+          </Flex>
+        )}
+        <Flex flex column>
+          <div className='gm-form-field'>
+            <FormControl>{children}</FormControl>
+            {!!(error && help) && (
+              <div className={classNames({ 'help-block': error })}>{help}</div>
+            )}
+          </div>
         </Flex>
-      )}
-      <Flex flex column>
-        <div className='gm-form-field'>
-          <FormControl>{children}</FormControl>
-          {!!(error && help) && (
-            <div className={classNames({ 'help-block': error })}>{help}</div>
-          )}
-        </div>
       </Flex>
-    </Flex>
-  )
-}
+    )
+  }
+)
 
-FormItemInner.propTypes = {
+FormItem.propTypes = {
   label: PropTypes.string,
   required: PropTypes.bool,
   unLabelTop: PropTypes.bool,
@@ -116,11 +116,6 @@ FormItemInner.propTypes = {
 
   className: PropTypes.string,
   style: PropTypes.object
-}
-
-const FormItem = props => {
-  const consumer = useContext(WrapContext)
-  return <FormItemInner {...consumer} {...props} />
 }
 
 FormItem.displayName = 'FormItem'
