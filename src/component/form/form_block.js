1b0f15c3349ef03652532d5f7c17bef5383d94cf
liyatang <liyatang@qq.com>
2019-08-09 17:15:41 +0800

    some

--- src/component/form/form_block.js
+++ src/component/form/form_block.js
@@ -1,39 +1,44 @@
-import React, { useContext } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 import Flex from '../flex'
 import _ from 'lodash'
 import classNames from 'classnames'
-import { withWrapContext, WrapContext, colWidth  } from './util'
+import { withWrapContext, WrapContext, colWidth } from './util'
 
-const FormBlock = withWrapContext(({ labelWidth, canValidate , children, className, col, style, ...rest }) => {
-  // 暂时
-  const _style = Object.assign({}, style, { width: colWidth * col })
+const FormBlock = withWrapContext(
+  ({ labelWidth, canValidate, children, className, col, style, ...rest }) => {
+    // 暂时
+    const _style = Object.assign({}, style, { width: colWidth * col })
 
-  return (
-    <WrapContext.Provider
+    return (
+      <WrapContext.Provider
         value={{
           labelWidth,
           canValidate
         }}
       >
-      <Flex
-        {...rest}
-        wrap
-        style={_style}
-        className={classNames('gm-form-block', className)}
-      >
-        {_.map(React.Children.toArray(children), (child, i) => {
-          return <Flex key={i} column>{child}</Flex>
-        })}
-      </Flex>
-    </WrapContext.Provider>
-  )
-})
+        <Flex
+          {...rest}
+          wrap
+          style={_style}
+          className={classNames('gm-form-block', className)}
+        >
+          {_.map(React.Children.toArray(children), (child, i) => {
+            return (
+              <Flex key={i} column>
+                {child}
+              </Flex>
+            )
+          })}
+        </Flex>
+      </WrapContext.Provider>
+    )
+  }
+)
 
 FormBlock.displayName = 'FormBlock'
 
 FormBlock.propTypes = {
-
   // 以下不要用， 由context传过来的
   labelWidth: PropTypes.string,
   canValidate: PropTypes.bool,
