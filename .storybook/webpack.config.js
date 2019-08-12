cbdf3c62ccc436a20f896820e908d29a7f31901e
liyatang <liyatang@qq.com>
2019-08-12 10:50:19 +0800

    fix babel-plugin-react-docgen

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,15 +1,38 @@
+const path = require('path')
+
 module.exports = ({ config }) => {
   // 不知道正则怎么写，用 function 代替
-  config.module.rules[0].exclude = function(filepath) {
-    if (
-      filepath.includes('/node_modules/gm-util/') ||
-      filepath.includes('/node_modules/gm-svg/')
-    ) {
-      return false
-    }
+  const rulesOne = {
+    test: /\.(mjs|jsx?)$/,
+    use: [
+      {
+        loader: 'babel-loader',
+        options: {
+          cacheDirectory: './node_modules/.cache/storybook',
+          babelrc: false,
+          plugins: [
+            [
+              path.resolve('./.storybook/babel-plugin-react-docgen/index.js'),
+              { DOC_GEN_COLLECTION_NAME: 'STORYBOOK_REACT_CLASSES' }
+            ]
+          ]
+        }
+      }
+    ],
+    include: [path.resolve('./')],
+    exclude: function(filepath) {
+      if (
+        filepath.includes('/node_modules/gm-util/') ||
+        filepath.includes('/node_modules/gm-svg/')
+      ) {
+        return false
+      }
 
-    return filepath.includes('/node_modules/')
+      return filepath.includes('/node_modules/')
+    }
   }
+
+  config.module.rules[0] = rulesOne
 
   config.module.rules[3] = {
     test: /\.(ico|jpg|jpeg|png|gif|eot|otf|webp|ttf|woff|woff2|cur|ani)(\?.*)?$/,
