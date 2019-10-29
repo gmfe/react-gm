e784abd428a5e621f7c3a96dec7b2625c09f2ae0
liyatang <liyatang@qq.com>
2019-10-30 07:52:11 +0800

    move box

---
+++ src/component/box/box_form.js
@@ -0,0 +1,61 @@
+import React, { useState } from 'react'
+import { Form } from '../form'
+import Flex from '../flex'
+import IconDownUp from '../icon_down_up'
+
+const BoxFormContext = React.createContext({
+  open: false
+})
+
+const More = props => {
+  return (
+    <BoxFormContext.Consumer>
+      {({ open }) => {
+        if (!open) {
+          return null
+        }
+        return props.children
+      }}
+    </BoxFormContext.Consumer>
+  )
+}
+
+const BoxForm = props => {
+  const [open, setOpen] = useState(false)
+
+  const handleToggle = () => {
+    setOpen(!open)
+  }
+
+  return (
+    <div className='gm-box gm-box-form'>
+      <Flex>
+        <Flex flex column>
+          <BoxFormContext.Provider
+            value={{
+              open
+            }}
+          >
+            <Form inline={!open} {...props}>
+              {props.children}
+            </Form>
+          </BoxFormContext.Provider>
+        </Flex>
+        <button
+          className='btn btn-link gm-padding-right-0'
+          onClick={handleToggle}
+        >
+          {open ? '收起' : ''}高级筛选 <IconDownUp active={open} />
+        </button>
+      </Flex>
+    </div>
+  )
+}
+
+BoxForm.More = More
+
+BoxForm.propTypes = {
+  ...Form.propTypes
+}
+
+export default BoxForm
